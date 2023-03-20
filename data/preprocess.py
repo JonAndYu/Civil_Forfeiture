60e281deea47acf42344d7072e9909d94704304e
Jonathan <jon.yu8888@gmail.com>
2023-03-19 17:41:47 -0700

    Add data

---
+++ data/preprocess.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+def main():
+    revenue = pd.read_csv("National_Revenue.csv")
+
+    categories = ["UNIT_TYPE", "PROP_TYPE"]
+
+    # For these two categories, there are cases like: "other", "OTHER", "Other"
+    # So we standardize them to be consistant with title labels "Other"
+    # Any np.nan value gets replace by "Other"
+    for category in categories:
+        revenue[category] = revenue[category].str.title()
+        revenue[category] = revenue[category].fillna("Other")
+
+    revenue["PROCD_TYPE"] = revenue["PROCD_TYPE"].str.title()
+    revenue["PROCD_TYPE"] = revenue["PROCD_TYPE"].fillna("Other")
+    revenue["PROCD_TYPE"] = revenue["PROCD_TYPE"].replace("Civil Jud", "Civil Judicial")
+    revenue["PROCD_TYPE"] = revenue["PROCD_TYPE"].replace("Civil ", "Civil")
+    revenue["PROCD_TYPE"] = revenue["PROCD_TYPE"].replace("Admin", "Administrative")
+
+    revenue["CONV_TYPE"] = revenue["CONV_TYPE"].str.title()
+    revenue["CONV_TYPE"] = revenue["CONV_TYPE"].fillna("Unknown")
+
+    revenue["CASE_NO"] = revenue["CASE_NO"].fillna("Unknown")
+
+    revenue["REV"] = revenue["REV"].fillna(0)
+
+    print(revenue[categories].nunique())
+    print(revenue.columns)
+
+    revenue.to_csv("processed_revenue.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
