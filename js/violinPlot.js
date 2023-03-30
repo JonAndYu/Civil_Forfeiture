141789d75b97efb98c1ac49cb5a686845cb5dde9
Jon <jon.yu8888@gmail.com>
2023-03-30 15:28:12 -0700

    most updated

--- js/violinPlot.js
+++ js/violinPlot.js
@@ -29,7 +29,7 @@ class ViolinPlot {
     updateVis() {
         let vis = this;
 
-        vis.data = vis._filterBetweenRangeOfYears(vis._filterNegativeRevenues(vis.data), 2010, 2016);
+        vis.data = vis._filterBetweenRangeOfYears(vis._filterNegativeRevenues(vis.data), 1986, 1991);
 
         vis._updateScales();
 
@@ -75,7 +75,9 @@ class ViolinPlot {
                 .classed('points', true)
                 .attr('r', 5)
 				.attr('cy', d => this.yScale(d["REV"]))
-				.attr('cx', d => this.xScale(d["YEAR"]) + 0.5 * vis.xScale.bandwidth() - this._addJitter(vis.xScale.bandwidth()))
+				//.attr('cx', d => this.xScale(d["YEAR"]) + 0.5 * vis.xScale.bandwidth() - this._addJitter(vis.xScale.bandwidth()))
+				.attr('cx', d => this.xScale(d["YEAR"]) + 0.5 * vis.xScale.bandwidth() - Math.abs(this._gaussianRandom()))
+                
                 .attr('class', d => {
                     console.log(d['PROP_TYPE']);
                     switch(d['PROP_TYPE']) {
@@ -195,5 +197,14 @@ class ViolinPlot {
      */
     _addJitter(binwidth) {
         return Math.random() * Math.floor(binwidth / 2) | 0;
+    }
+
+    _// Standard Normal variate using Box-Muller transform.
+    _gaussianRandom(mean=0, stdev=15) {
+        let u = 1 - Math.random(); // Converting [0,1) to (0,1]
+        let v = Math.random();
+        let z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+        // Transform to the desired mean and standard deviation:
+        return z * stdev + mean;
     }
 }
