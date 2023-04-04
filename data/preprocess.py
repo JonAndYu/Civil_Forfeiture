17cc3207efb4b2fddbbd457f50498f6b1d59e7e1
Jonathan <jon.yu8888@gmail.com>
2023-04-04 12:48:58 -0700

    Modified preprocess to remove year 0s

--- data/preprocess.py
+++ data/preprocess.py
@@ -26,6 +26,8 @@ def main():
 
     revenue["REV"] = revenue["REV"].fillna(0)
 
+    revenue.drop(revenue[(revenue['YEAR'] == 0)].index, inplace=True)
+
     print(revenue[categories].nunique())
     print(revenue.columns)
 
